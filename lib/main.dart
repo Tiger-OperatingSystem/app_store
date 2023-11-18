ab39fade55952b877d893ea42eab9e60c0586502
Augusto <auguzsto@gmail.com>
2023-11-18 06:23:45 -0300

    feat: patters

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:app_store/app_widget.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const AppWidget());
+}
---
+++ lib/src/main/main_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainView extends StatelessWidget {
+  const MainView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

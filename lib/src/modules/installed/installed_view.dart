b0613423ed388beea7bbb6148e2685c65cbbd352
Matheus Augusto <auguzsto@gmail.com>
2023-11-24 11:34:16 -0300

    feat: module installed

--- lib/src/modules/installed/installed_view.dart
+++ lib/src/modules/installed/installed_view.dart
@@ -1,12 +1,57 @@
+import 'dart:io';
+
+import 'package:app_store/src/modules/applications/applications_controller.dart';
+import 'package:app_store/src/modules/installed/installed_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class InstalledView extends StatelessWidget {
   const InstalledView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Packages installed'),
+    final installedController = InstalledController();
+
+    return ListenableBuilder(
+      listenable: context.watch<ApplicationsController>(),
+      builder: (context, child) {
+        return FutureBuilder(
+          future: installedController.flatpakInstalled(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            return ListView.builder(
+              shrinkWrap: true,
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final applicationModel = snapshot.data![index];
+
+                return ListTile(
+                  leading: Image.file(
+                    width: 35,
+                    height: 35,
+                    File(applicationModel.iconDesktopUrl!),
+                  ),
+                  title: Text(applicationModel.name!),
+                  subtitle: Text(applicationModel.summary!),
+                  trailing: SizedBox(
+                    height: 40,
+                    child: ElevatedButton(
+                        onPressed: () {
+                          context
+                              .read<ApplicationsController>()
+                              .removeFlatpak(applicationModel);
+                        },
+                        child: const Text("Remover")),
+                  ),
+                );
+              },
+            );
+          },
+        );
+      },
     );
   }
 }

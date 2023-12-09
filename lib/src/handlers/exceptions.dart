c7e8d3f11eced6223b0b39af5bd364056c1b8a1d
Matheus Augusto <auguzsto@gmail.com>
2023-12-09 13:31:30 -0300

    feat: button to report erros

--- lib/src/handlers/exceptions.dart
+++ lib/src/handlers/exceptions.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class CustomExceptionsWidget implements Exception {
   CustomExceptionsWidget(
@@ -9,12 +10,15 @@ class CustomExceptionsWidget implements Exception {
   }
 
   Future<void> _showHandler(String message, BuildContext context) async {
+    String uriReport = "https://github.com/Tiger-OperatingSystem/app_store/issues";
     showDialog(context: context, builder: (context) {
       return  AlertDialog(
         title: const Text("Ocorreu um erro."),
         content: Text(message),
         actions: [
-          ElevatedButton(onPressed: (){}, child: const Text("Reportar este problema")),
+          ElevatedButton(onPressed: () async {
+            launchUrl(Uri.parse(uriReport));
+          }, child: const Text("Reportar este problema")),
         ],
       );
     });

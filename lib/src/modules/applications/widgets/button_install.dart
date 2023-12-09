6f0ae76fc343f117f854e7491328783b4085c9d9
Matheus Augusto <auguzsto@gmail.com>
2023-12-09 14:01:44 -0300

    fix: remove package native

--- lib/src/modules/applications/widgets/button_install.dart
+++ lib/src/modules/applications/widgets/button_install.dart
@@ -64,7 +64,7 @@ class ButtonInstallWidget extends StatelessWidget {
                                 : const Icon(Icons.delete),
                             onPressed: () => _installOrRemoveFlatpak(
                               applicationModel,
-                              hasInstalledFlatpak,
+                              hasInstalledDebian,
                               applicationsController,
                               context,
                             ),

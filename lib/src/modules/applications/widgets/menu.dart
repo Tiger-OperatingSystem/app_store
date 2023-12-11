63fff6aa6c97281f7e2ea37521fc6c126b331ec2
Matheus Augusto <auguzsto@gmail.com>
2023-12-11 08:33:15 -0300

    fix: menu module applications

--- lib/src/modules/applications/widgets/menu.dart
+++ lib/src/modules/applications/widgets/menu.dart
@@ -6,58 +6,63 @@ class MenuWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Map<int, Widget> menu = {
-      0: const ItemMenuWidget(
+    List<Widget> menu = [
+      const ItemMenuWidget(
         title: "Populares",
         endpoint: "collection/popular",
         icon: Icons.auto_graph,
-      ),
-      1: const ItemMenuWidget(
+      ), 
+      const ItemMenuWidget(
         title: "Recem adicionados",
         endpoint: "collection/new",
         icon: Icons.celebration,
       ),
-      2: const ItemMenuWidget(
+      const ItemMenuWidget(
         title: "Recem atualizados",
         endpoint: "collection/recently-updated",
         icon: Icons.history,
       ),
-      3: const ItemMenuWidget(
+      const ItemMenuWidget(
         title: "Produtividade",
         endpoint: "category/Office",
         icon: Icons.work,
       ),
-      4: const ItemMenuWidget(
+      const ItemMenuWidget(
         title: "Gráficos e Fotografia",
         endpoint: "category/Graphics",
         icon: Icons.photo,
       ),
-      5: const ItemMenuWidget(
+      const ItemMenuWidget(
+        title: "Áudio & Vídeo",
+        endpoint: "category/AudioVideo",
+        icon: Icons.multitrack_audio,
+      ),
+      const ItemMenuWidget(
         title: "Educação",
         endpoint: "category/Education",
         icon: Icons.book,
       ),
-      6: const ItemMenuWidget(
+      const ItemMenuWidget(
         title: "Networking",
         endpoint: "category/Network",
         icon: Icons.work,
       ),
-      7: const ItemMenuWidget(
+      const ItemMenuWidget(
         title: "Desenvolvimento",
         endpoint: "category/Development",
         icon: Icons.build,
       ),
-      8: const ItemMenuWidget(
+      const ItemMenuWidget(
         title: "Ciência",
         endpoint: "category/Science",
         icon: Icons.biotech,
       ),
-      9: const ItemMenuWidget(
+      const ItemMenuWidget(
         title: "Utilitários",
         endpoint: "category/Utility",
         icon: Icons.system_update_tv_outlined,
       ),
-    };
+    ];
 
     return ListView.separated(
       itemCount: menu.length,

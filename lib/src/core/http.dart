2d280dda5ae85b4aa9e712ba68174069d819698b
Natanael Barbosa Santos <104917678+natanael-b@users.noreply.github.com>
2024-02-16 15:42:08 -0300

    Allow use jsons from Tiger OS repository

--- lib/src/core/http.dart
+++ lib/src/core/http.dart
@@ -3,14 +3,16 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class Http {
-  static const String _base = "https://flathub.org/api/v1/apps/";
+  static const String _flathub_base = "https://flathub.org/api/v1/apps/";
+  static const String _github_base  = "https://raw.githubusercontent.com/";
 
   static Future get(String route) async {
+    String _base = route.startsWith("Tiger-OperatingSystem/")?_github_base:_flathub_base;
     try {
-      final response = await http.get(Uri.parse("${Http._base}/$route"));
-
-      return jsonDecode(utf8.decode(response.body.codeUnits));
+      final response = await http.get(Uri.parse("$_base/$route"));
+      return jsonDecode(response.body);
     } catch (e) {
+      print(e); // Debug info
       rethrow;
     }
   }
